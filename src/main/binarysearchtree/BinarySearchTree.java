b469789d6f4ae561c66ce97768acd3b7ad1e8650
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-08-24 23:46:11 +0200

    Feature: Implemented contains method.

--- src/main/binarysearchtree/BinarySearchTree.java
+++ src/main/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package main.binarysearchtree;
 
+import org.graalvm.compiler.nodes.calc.RightShiftNode;
+
 public class BinarySearchTree<T extends Comparable<T>> {
 
 	// Tracks the number of nodes in this BST
@@ -66,67 +68,119 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 		return node;
 	}
-	
+
 	// Removes a value from the binary tree if it exists.
 	public boolean remove(T element) {
-		// Make sure that the node we want to remove actually exists in the binary tree before removing it.
-		if(true/*contains(element)*/) {
-			root=add(root, element);
+		// Make sure that the node we want to remove actually exists in the binary tree
+		// before removing it.
+		if (true/* contains(element) */) {
+			root = add(root, element);
 			nodeCount--;
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	private Node remove(Node node, T element) {
 		// Base case check
-		if(node==null) {
+		if (node == null) {
 			return null;
 		}
 		// determine in which direction do we need to go
-		int comp=element.compareTo(node.data);
-		
-		// Dig into left subtree. The node we are looking for is less than the current node value.
-		if(comp<0) {
-			node.left=remove(node.left, element);
+		int comp = element.compareTo(node.data);
+
+		// Dig into left subtree. The node we are looking for is less than the current
+		// node value.
+		if (comp < 0) {
+			node.left = remove(node.left, element);
 		}
-		// Dig into right sub-tree. The node we are looking for is greater than the current node value.
-		else if(comp>0) {
-			node.right=remove(node.right, element);
-		}
-		else {
+		// Dig into right sub-tree. The node we are looking for is greater than the
+		// current node value.
+		else if (comp > 0) {
+			node.right = remove(node.right, element);
+		} else {
 			// We found the node that we want to remove
-			// This is the case with a right sub tree or no subtree at all. In this situation, just swap the node we wish to remove with its right child.
-			if(node.left==null) {
-				Node rightChild=node.right;
-				node.data=null;
-				node=null;
+			// This is the case with a right sub tree or no subtree at all. In this
+			// situation, just swap the node we wish to remove with its right child.
+			if (node.left == null) {
+				Node rightChild = node.right;
+				node.data = null;
+				node = null;
 				return rightChild;
-			}else if(node.right==null) {
-				// This is the case with a left sub tree or no subtree at all. In this situation, just swap the node we wish to remove with its left child.
-				Node leftChild=node.left;
-				node.data=null;
-				node=null;
+			} else if (node.right == null) {
+				// This is the case with a left sub tree or no subtree at all. In this
+				// situation, just swap the node we wish to remove with its left child.
+				Node leftChild = node.left;
+				node.data = null;
+				node = null;
 				return leftChild;
-				
-			}else {
+
+			} else {
 				// The case when both nodes exists of the sub tree
-				// When removing a node from a binary tree with two links the successor of the node being removed can either be the largest value in the left subtree or the smallest value in the right subtree. In this implementation, I have decided to find the smallest value in the right subtree which can be found by travesing as far left as possible in the right subtree.
-				
+				// When removing a node from a binary tree with two links the successor of the
+				// node being removed can either be the largest value in the left subtree or the
+				// smallest value in the right subtree. In this implementation, I have decided
+				// to find the smallest value in the right subtree which can be found by
+				// travesing as far left as possible in the right subtree.
+
 				// Find the leftmostNode in the right sub tree.
-				Node tmp=null;//digLeft(node.right);
-				
+				Node tmp = digLeft(node.right);
+
 				// Swap the data
-				node.data=tmp.data;
-				
-				// Go into the right sub-tree and remove the leftmost node we found and swap data with. This prevents us from having two nodes in our tree with the same value.
-				node.right=remove(node.right, tmp.data);
+				node.data = tmp.data;
+
+				// Go into the right sub-tree and remove the leftmost node we found and swap
+				// data with. This prevents us from having two nodes in our tree with the same
+				// value.
+				node.right = remove(node.right, tmp.data);
 			}
-			
+
 		}
 		return node;
 	}
+
+	// Helper method to find the left most node
+	private Node digLeft(Node node) {
+		Node current = node;
+		while (current.left != null)
+			current = current.left;
+		return current;
+	}
+
+	// Helper method to find the right most node
+	private Node digRight(Node node) {
+		Node current = node;
+		while (current.right != null)
+			current = current.right;
+		return current;
+	}
+	
+	// Returns true if the element exists in the tree
+	public boolean contains(T element) {
+		return contains(root, element);
+	}
+	
+	// private recursive method to find an elemnt in a tree
+	private boolean contains(Node node, T element) {
+		//Base cae: element is null
+		if(node==null) {
+			return false;
+		}
+		int comp=element.compareTo(node.data);
+		
+		// Dig into left sub tree as the value of the element we are looking for is smaller than the current element
+		if(comp<0) {
+			return contains(node.left,element);
+		}else if(comp>0) {
+			// Dig into right sub tree as the value of the element we are looking for is greater than the current element
+			return contains(node.right, element);
+		}else {
+			// We found the element we are looking for.
+			return true;
+		}		
+	}
+	
 	
 	
 
