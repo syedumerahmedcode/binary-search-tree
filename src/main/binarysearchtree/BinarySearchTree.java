26021645b3be493b43838ffbcdb0dab003daa3bd
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-08-24 16:51:32 +0200

    Feature: Added method for adding nodes in a binary search tree.

--- src/main/binarysearchtree/BinarySearchTree.java
+++ src/main/binarysearchtree/BinarySearchTree.java
@@ -13,10 +13,10 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	private class Node{
 		T data;
 		Node left, right;
-		public Node(Node left, Node right, T data) {
+		public Node(Node left, Node right, T element) {
 			this.left=left;
 			this.right=right;
-			this.data=data;
+			this.data=element;
 		}
 	}
 	
@@ -29,6 +29,43 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	public Boolean isEmpty() {
 		return size()==0;
 	}
+	
+	/**
+	 * Adds an element to this binary tree.
+	 * @param element The element to add.
+	 * @return True if we successfully perfomr an insertion.
+	 */
+	public boolean add(T element) {
+		// Check if the element already exists in the binary tree.
+		// if it does, ignore adding it.
+		if(true/*contains(element)*/) {
+			return false;
+		}
+		//Otherwise add element to the binary tree.
+		else {
+			root=add(root, element);
+			nodeCount++;
+			return true;
+		}		
+	}
+	
+	// Privately method to recursively add a value in the binary tree.
+	private Node add(Node node, T element) {
+		// Base case: Found a leaft node
+		if(node==null) {
+			node=new Node(null, null, element);
+		}else {
+			// Place lower element values in the left sub-tree
+			if(element.compareTo(node.data)<0) {
+				node.left=add(node.left, element);
+			}else {
+				node.right=add(node.right, element);
+			}
+		}
+		return node;
+	}
+	
+	
 	
 	
 }
