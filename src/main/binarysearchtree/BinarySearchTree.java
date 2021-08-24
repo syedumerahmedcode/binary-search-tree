49fcd68fb18f34552e7430ebdb14a79a8d45a847
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-08-24 16:20:33 +0200

    Feature: Added node class as well as size() and isEmpty() methods in the Binary Search Tree class.

--- src/main/binarysearchtree/BinarySearchTree.java
+++ src/main/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,34 @@
 package main.binarysearchtree;
 
-public class BinarySearchTree {
+public class BinarySearchTree<T extends Comparable<T>> {
 
+	// Tracks the number of nodes in this BST
+	private int nodeCount=0;
+	
+	// This BST is a rooted tree so we keep track of the root node.
+	private Node root=null;
+	
+	// Internal node class containing 
+	// node references and the actual node data
+	private class Node{
+		T data;
+		Node left, right;
+		public Node(Node left, Node right, T data) {
+			this.left=left;
+			this.right=right;
+			this.data=data;
+		}
+	}
+	
+	// Get the number of nodes in this BST
+	public int size() {
+		return nodeCount;
+	}
+	
+	// Check if the Binary tree is empty
+	public Boolean isEmpty() {
+		return size()==0;
+	}
+	
+	
 }
